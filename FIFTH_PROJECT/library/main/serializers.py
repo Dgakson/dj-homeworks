14ce52824cfe287a775080e8b0201e66b17c58e0
Dgakson <www.jenya96@gmail.com>
2025-03-23 00:30:57 +0300

    add extra task to fifth task

--- FIFTH_PROJECT/library/main/serializers.py
+++ FIFTH_PROJECT/library/main/serializers.py
@@ -2,25 +2,34 @@ from rest_framework import serializers
 from .models import Book, Order
 
 
+class BookSerializerTest(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['author', 'title', 'year']
+
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
 
     #доп задание
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['orders_count'] = ...
-    #     return representation
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['orders_count'] = instance.orders.count()
+        return representation
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    books = BookSerializerTest(many=True, read_only=True)
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['id', 'user_name', 'days_count', 'date', 'books']
 
-    #доп задание
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['books'] = ...
-    #     return representation
+    # #доп задание
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        return representation
+
